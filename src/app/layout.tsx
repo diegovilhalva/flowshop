d031bd0c681c74c474f5d4afdb2f0ada4f693a81
Diego Vilhalva <diegorocha450@yahoo.com>
2024-10-17 12:58:20 -0300

    wix setup

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -9,7 +9,7 @@ export const metadata: Metadata = {
     template: "%s | Flow Shop",
     absolute: "Flow Shop",
   },
-  description: "A full-stack e-commerce application built with Next.js 15",
+  description: "Descubra a Flow Shop, sua loja online de moda e acessórios. Oferecemos uma ampla seleção de roupas, calçados e acessórios para todos os estilos. Com produtos de alta qualidade e preços acessíveis, a Flow Shop é o destino certo para renovar seu guarda-roupa com as últimas tendências. Explore nossa coleção e aproveite promoções exclusivas!",
 };
 
 export default function RootLayout({
@@ -18,7 +18,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="pt-br">
       <body className={lora.className}>{children}</body>
     </html>
   );
