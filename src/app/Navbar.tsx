26febde979a88fce1926c2618d41048c49bdec3f
Diego Vilhalva <diegorocha450@yahoo.com>
2024-10-24 08:43:46 -0300

    wix auth setup

--- src/app/Navbar.tsx
+++ src/app/Navbar.tsx
@@ -3,8 +3,10 @@ import Image from "next/image"
 import Link from "next/link"
 import logo from "@/assets/logo.png"
 import { getCart } from "@/wix-api/cart"
+import { getLoggedInMember } from "@/wix-api/members"
 import { getWixServerClient } from "@/lib/wix-client.server"
 import ShoppingCartButton from "./ShoppingCartButton"
+import UserButton from "@/components/UserButton"
 
 
 
@@ -12,19 +14,26 @@ import ShoppingCartButton from "./ShoppingCartButton"
 
 export default async function Navbar() {
 
-    const cart = await getCart(getWixServerClient())
+    const wixClient = getWixServerClient();
+    const [cart, loggedInMember] = await Promise.all([
+        getCart(wixClient),
+        getLoggedInMember(wixClient),
+    ]);
 
-    const totalQuantity = cart?.lineItems.reduce((acc,item) => acc + (item.quantity || 0),0) || 0
+
 
     return (
         <header className="bg-background shadow-sm">
             <div className="mx-auto flex max-w-7xl items-center justify-between gap-5 p-5">
                 <Link href={"/"} className="flex items-center gap-4">
-                <Image src={logo} alt="Flow Shop logo" width={40} height={40} />
-                <span className="text-xl font-bold">Flow Shop</span>
+                    <Image src={logo} alt="Flow Shop logo" width={40} height={40} />
+                    <span className="text-xl font-bold">Flow Shop</span>
                 </Link>
-                <ShoppingCartButton initialData={cart} />
-                
+                <div className="flex items-center justify-center gap-5">
+                    <UserButton loggedInMember={loggedInMember} />
+                    <ShoppingCartButton initialData={cart} />
+                </div>
+
             </div>
 
         </header>
