1a348ebd945090bddfaa527dcc003032c5fb54de
Diego Vilhalva <diegorocha450@yahoo.com>
2024-10-28 09:35:59 -0300

    responsive menu created

--- src/app/Navbar.tsx
+++ src/app/Navbar.tsx
@@ -10,6 +10,8 @@ import UserButton from "@/components/UserButton"
 import { getCollections } from "@/wix-api/collections"
 import MainNavigation from "./MainNavigation"
 import SearchField from "@/components/SearchField"
+import MobileMenu from "./MobileMenu"
+import { Suspense } from "react"
 
 
 
@@ -29,16 +31,22 @@ export default async function Navbar() {
     return (
         <header className="bg-background shadow-sm">
             <div className="mx-auto flex max-w-7xl items-center justify-between gap-5 p-5">
+                <Suspense>
+                    <MobileMenu
+                        collections={collections}
+                        loggedInMember={loggedInMember}
+                    />
+                </Suspense>
                 <div className="flex flex-wrap items-center gap-5">
                     <Link href={"/"} className="flex items-center gap-4">
                         <Image src={logo} alt="Flow Shop logo" width={40} height={40} />
                         <span className="text-xl font-bold">Flow Shop</span>
                     </Link>
-                    <MainNavigation collections={collections} />
+                    <MainNavigation collections={collections} className="hidden lg:flex" />
                 </div>
-                <SearchField className="max-w-96" />
+                <SearchField className="hidden max-w-96  lg:inline" />
                 <div className="flex items-center justify-center gap-5">
-                    <UserButton loggedInMember={loggedInMember} />
+                    <UserButton loggedInMember={loggedInMember} className="hidden lg:inline-flex" />
                     <ShoppingCartButton initialData={cart} />
                 </div>
 
