ba61b0a4dd12c5cb71e32668002ac4ba24cd46dc
Diego Vilhalva <diegorocha450@yahoo.com>
2024-10-29 11:02:45 -0300

    related products addd on produdct page

--- src/app/products/[slug]/page.tsx
+++ src/app/products/[slug]/page.tsx
@@ -1,44 +1,87 @@
 
-import { getProductBySlug } from "@/wix-api/products"
+import { getProductBySlug, getRelatedProducts } from "@/wix-api/products"
 import { notFound } from "next/navigation"
 import ProductDetails from "./ProductDetails"
 import { Metadata } from "next"
 import { delay } from "@/lib/utils"
 import { getWixServerClient } from "@/lib/wix-client.server"
+import { Suspense } from "react"
+import Product from "@/components/Product"
+import { Skeleton } from "@/components/ui/skeleton"
 
 interface PageProps {
-    params:{slug:string}
+    params: { slug: string }
 }
 
-export async function generateMetada({params:{slug}}:PageProps):Promise<Metadata>{
-    const product = await getProductBySlug(getWixServerClient(),slug)
+export async function generateMetada({ params: { slug } }: PageProps): Promise<Metadata> {
+    const product = await getProductBySlug(getWixServerClient(), slug)
     if (!product) notFound()
-    
+
     const mainImage = product.media?.mainMedia?.image
 
     return {
-        title:product.name,
-        description:"Compre esse produto na Flow Shop",
-        openGraph:{
-            images:mainImage?.url
-            ? [
-                {
-                    url:mainImage.url,
-                    width:mainImage.width,
-                    height:mainImage.height,
-                    alt:mainImage.altText || ""
-                }
-            ]: undefined, 
+        title: product.name,
+        description: "Compre esse produto na Flow Shop",
+        openGraph: {
+            images: mainImage?.url
+                ? [
+                    {
+                        url: mainImage.url,
+                        width: mainImage.width,
+                        height: mainImage.height,
+                        alt: mainImage.altText || ""
+                    }
+                ] : undefined,
         }
     }
 }
 
-export default async function Page({params:{slug}}:PageProps){
+export default async function Page({ params: { slug } }: PageProps) {
     await delay(3000)
-    const product = await getProductBySlug(getWixServerClient(),slug)
+    const product = await getProductBySlug(getWixServerClient(), slug)
 
-    if(!product?._id) notFound()
-       return (<main className="max-w-7xl mx-auto space-y-10 px-5 py-10">
-            <ProductDetails product={product} />
-        </main>)
+    if (!product?._id) notFound()
+    return (<main className="max-w-7xl mx-auto space-y-10 px-5 py-10">
+        <ProductDetails product={product} />
+        <hr />
+        <Suspense fallback={<RelatedProductsLoadingSkeleton/>}>
+            <RelatedProduts productId={product._id} />
+        </Suspense>
+    </main>)
+}
+
+interface RelatedProdutsProps {
+    productId: string
+}
+
+async function RelatedProduts({ productId }: RelatedProdutsProps) {
+    await delay(2000);
+
+    const relatedProducts = await getRelatedProducts(
+      getWixServerClient(),
+      productId,
+    );
+  
+    if (!relatedProducts.length) return null;
+  
+    return (
+      <div className="space-y-5">
+        <h2 className="text-2xl font-bold">Produtos relacionados</h2>
+        <div className="flex grid-cols-2 flex-col gap-5 sm:grid lg:grid-cols-4">
+          {relatedProducts.map((product) => (
+            <Product key={product._id} product={product} />
+          ))}
+        </div>
+      </div>
+    );
+}
+
+function RelatedProductsLoadingSkeleton() {
+    return (
+      <div className="flex grid-cols-2 flex-col gap-5 pt-12 sm:grid lg:grid-cols-4">
+        {Array.from({ length: 4 }).map((_, i) => (
+          <Skeleton key={i} className="h-[26rem] w-full" />
+        ))}
+      </div>
+    );
 }
